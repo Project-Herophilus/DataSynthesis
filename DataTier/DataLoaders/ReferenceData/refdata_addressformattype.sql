650ecd87209ed32ee1d6b47d562ffbe5e9005c26
balanscott <balanscott@outlook.com>
2021-07-02 19:14:48 -0500

    Reorganized Data Loaders to a single directory

---
+++ DataTier/DataLoaders/ReferenceData/refdata_addressformattype.sql
@@ -0,0 +1,6 @@
+INSERT INTO refdata_addressformattype (AddressFormatTypeDesc,CreatedDate,StatusId) VALUES 
+('US Based ','2020-07-21 01:13:08.0',1)
+,('International ','2020-07-21 01:13:08.0',1)
+,('United Kingdom','2020-07-21 01:13:08.0',1)
+,('Latin America','2020-07-21 01:13:08.0',1)
+;
