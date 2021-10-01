217923fa0003ee88382764504cde091c73ad186c
Jonathan Myer <jonathan.myer@ibm.com>
2021-10-01 12:50:59 -0400

    added pathing for env filel

--- DataTier-DataBuilder/db-connections/config.js
+++
@@ -1,7 +0,0 @@
-const datasynthesis = {
-    mysql_host: '54.156.72.120',
-    mysql_user:'datasynthesis',
-    mysql_password:'@Data%9F',
-    mysql_db:'datasynthesis'
-}
-module.exports = datasynthesis
--- DataTier-DataBuilder/db-connections/mysql.js
+++ DataTier-DataBuilder/db-connections/mysql.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
-const dbconfig = require('./config');
-
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 const con = mysql.createConnection({
-  host: dbconfig.mysql_host,
-  user: dbconfig.mysql_user,
-  password: dbconfig.mysql_password,
-  database: dbconfig.mysql_db,
+  host: process.env.mysql_host,
+  user: process.env.mysql_user,
+  password: process.env.mysql_password,
+  database: process.env.mysql_db,
   multipleStatements: true,
   queryTimeout: 6000,
   connectTimeout: 60000
