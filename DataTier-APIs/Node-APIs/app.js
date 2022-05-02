da326df577c66239008431692cf1165605e87957
Andrew Baumann <777ortale15@gmail.com>
2022-05-01 22:55:19 -0500

    OpenAPI initial addition (#37)
    Co-authored-by: Andrew Baumann <andrew@mrbaumann.me>

--- DataTier-APIs/Node-APIs/app.js
+++ DataTier-APIs/Node-APIs/app.js
@@ -16,6 +16,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
     //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     // Pass to next layer of middleware
     next();
