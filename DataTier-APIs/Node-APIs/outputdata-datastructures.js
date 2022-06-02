81f20bd9a9cb6e93875c49426e69976162e341b8
balanscott <balanscott@live.com>
2022-06-02 01:24:18 -0500

    Updates:
    - enhanced outputdata-datastructures.js and buildComplexDataStructures.js to include the newly updated table name platform_config_datastructures and platform_config_datastructures_dtl
    
    - Tweaked Dockerfile
    
    - Enhanced reusableQueries.js

--- DataTier-APIs/Node-APIs/outputdata-datastructures.js
+++ DataTier-APIs/Node-APIs/outputdata-datastructures.js
@@ -26,21 +26,26 @@ let datastructureName;
 let systemOutputName;
 // Global Variable for usage in platform
 global.__basedir = __dirname;
+const args = process.argv.slice(2);
 
 /* DataStructureName
  * This must match the name in the platform_datastructures table from the field
  * datastructurename
  */
 //datastructureName ="Person Demographics";
-datastructureName ="Complete Name";
+//datastructureName ="Complete Name";
 //datastructureName ="US Address";
 //datastructureName ="Bank Account";
-//datastructureName ="US Phone Number";
+
+datastructureName = args[0];
+if (datastructureName  == null)
+{
+    datastructureName  = "Person Demographics";
+}
 
 const appName="DataSynthesis";
 const appGUID=uuid.v4();
 const runQuantity = 5000;
-
 componentName = "buildComplexDataStructures";
 // Set Start Value for timing
 let startTime = new Date();
@@ -55,20 +60,20 @@ buildComplexDataStructures.buildComplexDataStructure(datastructureName, runQuant
     resp.forEach(msg=>{
         const dataObject = {"date":new Date(),"applicationName":appName,"appGUID":appGUID,
             "componentName": componentName,"methodName": methodName,"data":msg}
-        finalDataOutPut.push(dataObject)
+        finalDataOutPut.push(JSON.stringify(dataObject))
     })
-    endTime = new Date();
+    // endTime = new Date();
     // Auditing - Publish
-    auditing.generate_auditrecord(runQuantity,componentName,appName,startTime,endTime);
+    // auditing.generate_auditrecord(runQuantity,componentName,appName,startTime,endTime);
     // Auditing Values
-    startTime = new Date();
+    // startTime = new Date();
     // Output Record
     //externalizeDataOutput(finalDataOutPut, outputType)
-    dataOutputting.processDataOutput(finalDataOutPut, datastructureName);
+    dataOutputting.processDataOutput(finalDataOutPut, methodName);
     // Audit
-    endTime = new Date();
-    componentName = "DataOutput";
-    auditing.generate_auditrecord(runQuantity,componentName,appName,startTime,endTime);
+    // endTime = new Date();
+    // componentName = "DataOutput";
+    // auditing.generate_auditrecord(runQuantity,componentName,appName,startTime,endTime);
 })
 .catch(err=>{
     console.log(err)
