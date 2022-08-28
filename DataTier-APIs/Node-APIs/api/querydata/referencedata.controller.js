f5061b838e041fd9cd88508f6c2e0801b01ee13c
balanscott <balanscott@live.com>
2022-08-28 02:04:08 -0400

    Updates:
    - Enhanced Existing data API, this involves all the referencedata.controller.js code to ensure that better json responses were provided, when data is not returned a consistent message is returned with some details for the requestor
    - Enhanced Existing data API, this involves all the termsdata.controller.js code to ensure that better json responses were provided, when data is not returned a consistent message is returned with some details for the requestor

--- DataTier-APIs/Node-APIs/api/querydata/referencedata.controller.js
+++ DataTier-APIs/Node-APIs/api/querydata/referencedata.controller.js
@@ -10,93 +10,184 @@ let rdbmsType = process.env.rdbms;
 */
 
 router.get('/codesets', function (req, res) {
+    let sqlQuery = 'select * from refdata_codeset';
     dbConnection.query('select * from refdata_codeset', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/countries', function (req, res) {
+    let sqlQuery = 'select * from refdata_countries';
     dbConnection.query('select * from refdata_countries', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/datagentypes', function (req, res) {
+    let sqlQuery = 'select * from refdata_datagentypes';
     dbConnection.query('select * from refdata_datagentypes', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/industrystd', function (req, res) {
+    let sqlQuery = 'select * from refdata_industrystd';
     dbConnection.query('select * from refdata_industrystd', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/operationtype', function (req, res) {
+    let sqlQuery = 'select * from refdata_operationtype';
     dbConnection.query('select * from refdata_operationtype', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/platformparams', function (req, res) {
+    let sqlQuery = 'select * from refdata_platformparams';
     dbConnection.query('select * from refdata_platformparams', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/platformparamstodataattributes', function (req, res) {
+    let sqlQuery = 'select * from refdata_platformparamstodataattributes';
     dbConnection.query('select * from refdata_platformparamstodataattributes', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/sensitivityflags', function (req, res) {
+    let sqlQuery = 'select * from refdata_sensitivityflag';
     dbConnection.query('select * from refdata_sensitivityflag', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/statuses', function (req, res) {
+    let sqlQuery = 'select * from refdata_status';
     dbConnection.query('select * from refdata_status', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/terminologystd', function (req, res) {
+    let sqlQuery = 'select * from refdata_terminologystd';
     dbConnection.query('select * from refdata_terminologystd', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/timezones', function (req, res) {
+    let sqlQuery = 'select * from refdata_timezones';
     dbConnection.query('select * from refdata_timezones', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/usstates', function (req, res) {
+    let sqlQuery = 'select * from refdata_usstates';
     dbConnection.query('select * from refdata_usstates', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/vendors', function (req, res) {
+    let sqlQuery = 'select * from refdata_vendor';
     dbConnection.query('select * from refdata_vendor', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
--- DataTier-APIs/Node-APIs/api/querydata/termsdata.controller.js
+++ DataTier-APIs/Node-APIs/api/querydata/termsdata.controller.js
@@ -10,30 +10,60 @@ let rdbmsType = process.env.rdbms;
 */
 
 router.get('/termscodesethl7v2', function (req, res) {
+    let sqlQuery = 'select * from terms_codeset_HL7v2';
     dbConnection.query('select * from terms_codeset_HL7v2', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/termscodesethl7v2/:code', function (req, res) {
-    dbConnection.query('select * from terms_codeset_HL7v2 and code like ?', [req.params.code], function (error, results, fields) {
+    const codeVal = req.params.code;
+    let sqlQuery = 'select * from terms_codeset_HL7v2 where code like '+'%'+codeVal+'%';
+    dbConnection.query('select * from terms_codeset_HL7v2 where code like ?', [req.params.code], function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/termscodesetumls', function (req, res) {
+    let sqlQuery = 'select * from terms_umls_mrconoso';
     dbConnection.query('select * from terms_umls_mrconoso', function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
 router.get('/termscodesetumls/:sabcode', function (req, res) {
-    dbConnection.query('select * from terms_umls_mrconoso SAB=?', [req.params.sabcode], function (error, results, fields) {
+    const codeVal = req.params.sabcode;
+    let sqlQuery = 'select * from terms_umls_mrconoso where SAB like '+'%'+codeVal+'%';
+    dbConnection.query('select * from terms_umls_mrconoso where SAB=?', [req.params.sabcode], function (error, results, fields) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        if (results.rows.length > 0){
+            res.end(JSON.stringify(results.rows));
+            res.status(200).send();
+        }
+        else {
+            res.status(500).send("No Data Available"+strQuery);
+        }
     });
 });
 
